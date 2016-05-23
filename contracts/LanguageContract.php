66a14e57fac89a83df66cffb2e669a9d75a1ca1e
chapal <siarhei.sharykhin@gmail.com>
2016-05-24 00:19:28 +0300

    move api calls throught private variable. Thinks about polymorphism

---
+++ contracts/LanguageContract.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chapal
+ * Date: 23.5.16
+ * Time: 23.58
+ */
--- exceptions/LanguageException.php
+++ exceptions/LanguageException.php
@@ -10,40 +10,26 @@ use Support\Codes;
  */
 class LanguageFileException extends \LogicException
 {
-    /** @var string $application */
-    private $application;
     /** @var string $language  */
     private $language;
 
     /**
      * LanguageFileException constructor.
-     * @param string $application
-     * @param int $language
+     * @param string $language
      * @param \Exception $message
      * @param $code
      * @param \Exception $previous
      */
-    public function __construct(
-        $application,
+    public function __construct(      
         $language,
         $message,
         $code = Codes::LANGUAGE_FILE_ERROR,
         \Exception $previous = null)
-    {
-        $this->application = $application;
+    {        
         $this->language = $language;
-
         parent::__construct($message, $code, $previous);
     }
 
-    /**
-     * Return an application
-     * @return string
-     */
-    public function getApplication()
-    {
-        return $this->application;
-    }
 
     /**
      * Return a language
